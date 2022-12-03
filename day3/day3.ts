bb848a1766cdda5906a3f3eac8b29f43dd73e2f8
Oskar Rubensson <107415465+osru-knightec@users.noreply.github.com>
2022-12-03 11:10:34 +0100

    Day 3

---
+++ day3/day3.ts
@@ -0,0 +1,41 @@
+import { readFileSync } from 'fs';
+const dummyData = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
+
+const day3 = () => {
+  const data = readFileSync('day3/day3-input', 'utf8').trimEnd();
+
+  /** Part 1 */
+  const part1 = data
+    .split('\n')
+    .map((line) => [
+      line.slice(0, line.length / 2),
+      line.slice(line.length / 2),
+    ])
+    .map(([a, b]) => a.split('').find((i) => b.includes(i)) || 'a')
+    .map((i) =>
+      i.charCodeAt(0) > 90 ? i.charCodeAt(0) - 96 : i.charCodeAt(0) - 38,
+    )
+    .reduce((a, b) => a + b, 0);
+  console.log('Part 1: ', part1);
+
+  /** Part 2 */
+  const part2 = data
+    .split('\n')
+    .map((line, i, lines) => [line, lines[i + 1], lines[i + 2]])
+    .filter((line, index) => index % 3 === 0)
+    .map(
+      ([a, b, c]) =>
+        a.split('').find((i) => b?.includes(i) && c?.includes(i)) || 'a',
+    )
+    .map((i) =>
+      i.charCodeAt(0) > 90 ? i.charCodeAt(0) - 96 : i.charCodeAt(0) - 38,
+    )
+    .reduce((a, b) => a + b, 0);
+  console.log('Part 2: ', part2);
+};
+day3();
