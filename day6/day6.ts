d0f37769ee93851199c6b1bf9ca6818a3ebe695a
Oskar Rubensson <107415465+osru-knightec@users.noreply.github.com>
2022-12-06 09:13:57 +0100

    Day 6

---
+++ day6/day6.ts
@@ -0,0 +1,44 @@
+import { readFileSync } from 'fs';
+
+const day6 = () => {
+  const input = readFileSync('day6/day6-input', 'utf8');
+
+  /** Part 1 */
+  const part1 = input.split('').reduce(
+    ({ acc, i }, curr, index) => {
+      if (
+        index > 3 &&
+        !acc.split('').find((c) => acc.lastIndexOf(c) !== acc.indexOf(c))
+      ) {
+        return { acc, i };
+      }
+      return {
+        acc: acc.length < 4 ? acc + curr : acc.substring(1) + curr,
+        i: index + 1,
+      };
+    },
+    { acc: '', i: 0 },
+  );
+
+  console.log('Part 1: ', part1);
+
+  /** Part 2 */
+  const part2 = input.split('').reduce(
+    ({ acc, i }, curr, index) => {
+      if (
+        index > 14 &&
+        !acc.split('').find((c) => acc.lastIndexOf(c) !== acc.indexOf(c))
+      ) {
+        return { acc, i };
+      }
+      return {
+        acc: index < 14 ? acc + curr : acc.substring(1) + curr,
+        i: index + 1,
+      };
+    },
+    { acc: '', i: 0 },
+  );
+  console.log('Part 2: ', part2);
+};
+
+day6();
